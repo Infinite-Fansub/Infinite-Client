9502e268a2897abf09c7d7c3abdb7b025378d547
DidaS <80978939+Didas-git@users.noreply.github.com>
2021-11-29 10:14:24 +0000

    Remade the handler and recursive read
    Huge thanks to @Nightmaretopia for making the recursive read

--- src/utils/handler.ts
+++ src/utils/handler.ts
@@ -13,29 +13,24 @@ export default class Handler {
     }
 
     public loadCommands(dir: string = this.dirs?.commands!) {
-        recursiveRead(dir, (err, result) => {
-            if (err) throw err;
-            result?.forEach(async (path) => {
+        recursiveRead(dir)
+            .forEach(async (path) => {
                 const command = (await import(path)).default;
                 this.client.commands.set(command.data.name, command)
             })
-        })
     }
 
     public loadSlashCommands(dir: string = this.dirs?.slashCommands!) {
-        recursiveRead(dir, (err, result) => {
-            if (err) throw err;
-            result?.forEach(async (path) => {
+        recursiveRead(dir)
+            .forEach(async (path) => {
                 const command = (await import(path)).default;
                 this.client.slashCommands.set(command.data.name, command)
             })
-        })
     }
 
     public loadEvents(dir: string = this.dirs?.events!) {
-        recursiveRead(dir, (err, result) => {
-            if (err) throw err;
-            result?.forEach(async (path) => {
+        recursiveRead(dir)
+            .forEach(async (path) => {
                 const event = (await import(path)).default;
                 this.client.events.set(event.event, event)
                 this.client[event.type as "on" | "once"](event.event, (...args) => {
@@ -43,6 +38,5 @@ export default class Handler {
                     event.run(...args)
                 })
             })
-        })
     }
 }
