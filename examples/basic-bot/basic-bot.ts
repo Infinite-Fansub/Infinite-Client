4c47299a889ddde678c1471274b28de9606c27ba
DidaS <didasoficial@gmail.com>
2021-12-22 14:22:59 +0000

    Fixed the problem

---
+++ examples/basic-bot/basic-bot.ts
@@ -0,0 +1,14 @@
+import { InfiniteClient } from "../../src";
+import { token, mongoPath } from "../../config.json";
+import { join } from "path"
+
+new InfiniteClient(token, {
+    intents: 32511,
+    partials: ['USER', 'REACTION', 'MESSAGE', 'GUILD_MEMBER', 'CHANNEL'],
+    useDatabase: true,
+    databaseType: { type: "mongo", mongoPath },
+    dirs: {
+        slashCommands: join(__dirname, "./slashCommands"),
+        events: join(__dirname, "./events")
+    }
+})
