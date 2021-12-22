75e3e9d7ff07fb66bc5c8be91f87654b05e763b5
DidaS <80978939+Didas-git@users.noreply.github.com>
2021-12-22 14:18:47 +0000

    Update basic-bot.ts

--- examples/basic-bot/basic-bot.ts
+++ examples/basic-bot/basic-bot.ts
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream:examples/basic-bot/test.ts
-import InfiniteClient from "../../src/client";
-import { token } from "../../config.json";
-=======
 import { InfiniteClient } from "../../src/index";
 import { token, mongoPath } from "../../config.json";
->>>>>>> Stashed changes:examples/basic-bot/basic-bot.ts
 import { join } from "path"
 
 new InfiniteClient(token, {
