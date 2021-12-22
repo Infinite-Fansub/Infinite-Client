4937c254068a93e36f1691b40ab36f7bbf67472c
DidaS <didasoficial@gmail.com>
2021-12-22 15:14:09 +0000

    Removed some imports

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,9 @@
-import { Color, ColorSpace, colorConsole, Interpolation } from "colours.js";
+import { Color, colorConsole } from "colours.js";
 import { DefaultColors, Emojis } from "../types";
 import { getCurrentMemoryHeap } from "./memory-heap";
 import { InfiniteGradient } from "./colors/infinite"
 
-export default class Logger {
+export class Logger {
     private emoji: string = "💫";
     private errorEmoji: string = "❌";
     private defaultColors: DefaultColors = {
@@ -13,23 +13,23 @@ export default class Logger {
         gradientSecondary: Color.fromHex("#c603fc")
     };
 
-    setEmojis(emojis: Emojis): void {
+    public setEmojis(emojis: Emojis): void {
         if (emojis.emoji) this.emoji = emojis.emoji;
         if (emojis.errorEmoji) this.errorEmoji = emojis.errorEmoji;
     };
 
-    date(): string {
+    public date(): string {
         return colorConsole.uniform(colorConsole.uniform(`[${new Date().toLocaleTimeString()}]`, Color.WHITE, true), Color.BLACK)
     };
 
-    defaultPrint(log: string, showMemory: boolean = false): void {
+    public defaultPrint(log: string, showMemory: boolean = false): void {
 
         return showMemory
             ? console.log(`${colorConsole.gradient(getCurrentMemoryHeap(), InfiniteGradient())} ${this.date()} ${this.emoji} ${colorConsole.gradient(log, InfiniteGradient(true))}`)
             : console.log(`${this.date()} ${this.emoji} ${colorConsole.uniform(log, this.defaultColors.color)}`)
     };
 
-    error(log: string, showMemory: boolean = false): void {
+    public error(log: string, showMemory: boolean = false): void {
         return showMemory
             ? console.error(`${colorConsole.uniform(getCurrentMemoryHeap(), this.defaultColors.errorColor)} ${this.date()} ${this.errorEmoji} ${colorConsole.uniform(log, this.defaultColors.errorColor)}`)
             : console.error(`${this.date()} ${this.errorEmoji} ${colorConsole.uniform(log, this.defaultColors.errorColor)}`)
