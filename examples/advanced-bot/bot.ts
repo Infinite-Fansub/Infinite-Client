fdc0859e1ab48593bde6ed076bad2f972bce4fe9
ChasJC23 <chas.coxell@gmail.com>
2022-02-28 21:35:03 +0000

    :warning: example update :warning:

--- examples/advanced-bot/bot.ts
+++ examples/advanced-bot/bot.ts
@@ -6,7 +6,7 @@ new InfiniteClient(token, {
     intents: 32511,
     partials: ['USER', 'REACTION', 'MESSAGE', 'GUILD_MEMBER', 'CHANNEL'],
     useDatabase: true,
-    databaseType: { type: "mongo", mongoPath },
+    databaseType: { type: "mongo", path: mongoPath },
     dirs: {
         slashCommands: join(__dirname, "./slashCommands"),
         events: join(__dirname, "./events")
